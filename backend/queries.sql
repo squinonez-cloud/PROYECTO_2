0114226546c8aa1edac345de6847de5d8fd88e43
alex-oro0809 <aorozco2@colegiovalledelsaber.edu.gt>
2026-09-18 18:10:05 -0600

    Create queries.sql

---
+++ backend/queries.sql
@@ -0,0 +1,7 @@
+SELECT 
+  s.id AS id,
+  u.nombre AS usuario,
+  DATE_FORMAT(s.fecha_entrada, '%Y-%m-%d %H:%i') AS fechaEntrada,
+  DATE_FORMAT(s.fecha_salida, '%Y-%m-%d %H:%i') AS fechaSalida
+FROM sesiones s
+JOIN usuarios u ON s.usuario_id = u.id;
